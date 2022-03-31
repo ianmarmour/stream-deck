32eb40a8cade75c2e28c7399518bc5e192772224
Ian Armour <ianmarmour@gmail.com>
2022-03-30 18:57:09 -0700

    Testing commit for cross compiling on steam deck

--- main.rs
+++ main.rs
@@ -1,5 +1,7 @@
+use gstreamer::prelude::*;
 use wayland_client::{Display, GlobalManager, global_filter };
 use gamescope_protocol::gamescope_pipewire::client::gamescope_pipewire::{GamescopePipewire, Event};
+use anyhow::Error;
 
 fn main() {
     let wl_gamescope_display_name = "gamescope-0";
@@ -17,25 +19,61 @@ fn main() {
 
     gamescope.quick_assign(|_main, ev, _dispatch_data| {
         match ev {
-            Event::StreamNode { node_id } => print_gamescope_pipewire_id(node_id),
-            _ => println!("{}", "foo")
+            Event::StreamNode { node_id } => start_streaming(node_id).unwrap(),
+            _ => (),
         }
     });
 
     event_queue.sync_roundtrip(&mut (), |_, _, _| unreachable!()).unwrap();
 }
 
-fn print_gamescope_pipewire_id(id: u32) {
-    println!("{}", id)
-}
+fn start_streaming(id: u32) -> Result<(), Error> {
+    gstreamer::init()?;
 
-fn build_ui(id: u32) {
-    let window = gtk::ApplicationWindow::new(application);
+    let pipeline = gstreamer::parse_launch(&format!("pipewiresrc path={} ! videoconvert ! xvimagesink", id))?;
 
-    window.set_title("First GTK+ Clock");
-    window.set_border_width(10);
-    window.set_position(gtk::WindowPosition::Center);
-    window.set_default_size(260, 40);
-
-    window.show_all();
+       // Start playing
+       let main_loop = glib::MainLoop::new(None, false);
+       let main_loop_clone = main_loop.clone();
+       let pipeline_weak = pipeline.downgrade();
+       let bus = pipeline.bus().expect("Pipeline has no bus");
+       bus.add_watch(move |_, msg| {
+           let pipeline = match pipeline_weak.upgrade() {
+               Some(pipeline) => pipeline,
+               None => return glib::Continue(true),
+           };
+           let main_loop = &main_loop_clone;
+           match msg.view() {
+               gstreamer::MessageView::Error(err) => {
+                   println!(
+                       "Error from {:?}: {} ({:?})",
+                       err.src().map(|s| s.path_string()),
+                       err.error(),
+                       err.debug()
+                   );
+                   let _ = pipeline.set_state(gstreamer::State::Ready);
+                   main_loop.quit();
+               }
+               gstreamer::MessageView::Eos(..) => {
+                   // end-of-stream
+                   let _ = pipeline.set_state(gstreamer::State::Ready);
+                   main_loop.quit();
+               }
+               gstreamer::MessageView::ClockLost(_) => {
+                   // Get a new clock
+                   let _ = pipeline.set_state(gstreamer::State::Paused);
+                   let _ = pipeline.set_state(gstreamer::State::Playing);
+               }
+               _ => (),
+           }
+           glib::Continue(true)
+       })
+       .expect("Failed to add bus watch");
+   
+       main_loop.run();
+   
+       bus.remove_watch()?;
+       pipeline.set_state(gstreamer::State::Null)?;
+   
+       Ok(())
 }
